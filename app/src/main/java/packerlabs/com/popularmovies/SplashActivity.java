44ee63a3a8b6af503ecb9258c857697ad2c06cfb
Jeremy <AdminJpack@ws246204.oerd-dhcp.siu.edu>
2017-01-04 16:41:35 -0600

    added extra second load

--- app/src/main/java/packerlabs/com/popularmovies/SplashActivity.java
+++ app/src/main/java/packerlabs/com/popularmovies/SplashActivity.java
@@ -22,6 +22,6 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        }, 1000);
+        }, 2000);
     }
 }
